cb711e4151455173e070b29acf8d06535fd69808
Eden Mesfun <emesfun@gmail.com>
2018-03-14 16:36:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,6 +42,9 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 
+    DB[:conn].each do |row|
+      
+    end #each iterator
 
   end
 
