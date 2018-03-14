241cc67c6e8e96167a2e93b62e4f558a67f79da3
Eden Mesfun <emesfun@gmail.com>
2018-03-14 16:01:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,9 +5,9 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize
+  def initialize(name, grade, id = nil)
   end
-  
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
