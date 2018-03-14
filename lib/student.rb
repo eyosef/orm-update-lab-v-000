587dc75dd495b5fdb423572629917597960097b1
Eden Mesfun <emesfun@gmail.com>
2018-03-14 16:12:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,10 @@ class Student
   def self.create_table
 
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS
+      CREATE TABLE IF NOT EXISTS students
+      id INTEGER PRIMARY KEY
+      name TEXT
+      grade INTEGER
     SQL
   end
 
